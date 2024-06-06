3dff0f1294583ef12c3db3ddb61e56cbeb3fd915
Amit Bhanja <amit.bhanja@sixsq.com>
2024-06-06 10:58:38 +0200

    Remove some comments

--- nuvlaedge-http.go
+++ nuvlaedge-http.go
@@ -136,18 +136,11 @@ func handleUnmatchedContentType(resp http.ResponseWriter) {
 }
 
 func GetHTTPStatusCodeFromStatus(s *status.Status) int {
-	// See https://github.com/open-telemetry/opentelemetry-proto/blob/main/docs/specification.md#failures
-	// to see if a code is retryable.
-	// See https://github.com/open-telemetry/opentelemetry-proto/blob/main/docs/specification.md#failures-1
-	// to see a list of retryable http status codes.
 	switch s.Code() {
-	// Retryable
 	case codes.Canceled, codes.DeadlineExceeded, codes.Aborted, codes.OutOfRange, codes.Unavailable, codes.DataLoss:
 		return http.StatusServiceUnavailable
-	// Retryable
 	case codes.ResourceExhausted:
 		return http.StatusTooManyRequests
-	// Not Retryable
 	default:
 		return http.StatusInternalServerError
 	}
@@ -155,8 +148,6 @@ func GetHTTPStatusCodeFromStatus(s *status.Status) int {
 
 func NewStatusFromMsgAndHTTPCode(errMsg string, statusCode int) *status.Status {
 	var c codes.Code
-	// Mapping based on https://github.com/grpc/grpc/blob/master/doc/http-grpc-status-mapping.md
-	// 429 mapping to ResourceExhausted and 400 mapping to StatusBadRequest are exceptions.
 	switch statusCode {
 	case http.StatusBadRequest:
 		c = codes.InvalidArgument
